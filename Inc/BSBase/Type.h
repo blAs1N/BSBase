f6305f61ba805fa91f71c39efc0856705083836a
blAs1N <qazasa123@gmail.com>
2021-04-08 00:46:45 +0900

    Fix wrong type

--- Inc/BSBase/Type.h
+++ Inc/BSBase/Type.h
@@ -8,7 +8,7 @@
 namespace BSBase
 {
 	using uint8 = unsigned char;
-	using uint16 = unsigned char;
+	using uint16 = unsigned short;
 	using uint32 = unsigned int;
 	using uint64 = unsigned long long;
 
