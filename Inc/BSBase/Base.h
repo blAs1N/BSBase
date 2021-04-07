5a3007238256e4590328641f3dcf30717bebf57b
blAs1N <qazasa123@gmail.com>
2021-04-08 00:47:03 +0900

    Add string helper macro

--- Inc/BSBase/Base.h
+++ Inc/BSBase/Base.h
@@ -12,8 +12,14 @@
 #	define NULL ((void*)0)
 #endif
 
-#define STRINGIFYIMPL(x) #x
-#define STRINGIFY(x) STRINGIFYIMPL(x)
+#define ADD_PREFIX_IMPL(prefix, x) prefix##x
+#define ADD_PREFIX(prefix, x) ADD_PREFIX_IMPL(prefix, x)
+
+#define STRINGIFY_PREFIX_IMPL(prefix, x) prefix#x
+#define STRINGIFY_PREFIX(prefix, x) STRINGIFY_PREFIX_IMPL(prefix, x)
+
+#define STRINGIFY_IMPL(x) #x
+#define STRINGIFY(x) STRINGIFY_IMPL(x)
 
 #define STRING_JOIN(lhs, rhs)  STRING_JOIN1(lhs, rhs)
 #define STRING_JOIN1(lhs, rhs) STRING_JOIN2(lhs, rhs)
