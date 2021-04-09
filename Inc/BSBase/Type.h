8408d9e788ad06381729da5eb5a91b45321ad838
blAs1N <qazasa123@gmail.com>
2021-04-09 23:04:37 +0900

    Remove nullptr_t, size_t, ssize_t, ptrdiff_t

--- Inc/BSBase/Type.h
+++ Inc/BSBase/Type.h
@@ -1,10 +1,5 @@
 #pragma once
 
-#if !defined(x64) && !defined(x86)
-#	include <cstddef>
-#	include <type_traits>
-#endif
-
 namespace BSBase
 {
 	using uint8 = unsigned char;
@@ -16,20 +11,4 @@ namespace BSBase
 	using int16 = short;
 	using int32 = int;
 	using int64 = long long;
-
-	using nullptr_t = decltype(nullptr);
-
-#if defined(x64)
-	using size_t = uint64;
-	using ssize_t = int64;
-	using ptrdiff_t = int64;
-#elif defined(x86)
-	using size_t = uint32;
-	using ssize_t = int32;
-	using ptrdiff_t = int32;
-#else
-	using size_t = ::std::size_t;
-	using ssize_t = ::std::make_signed_t<size_t>;
-	using ptrdiff_t = ::std::ptrdiff_t;
-#endif
 }
